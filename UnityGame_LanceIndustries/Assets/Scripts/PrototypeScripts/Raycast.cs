e21b9327cef2d1853d95045f54b2d87fa549772c
SaktiNash23 <sativel@hotmail.com>
2020-03-29 15:09:02 +0800

    Cleaned Version after learning how to resolve conflicts

--- UnityGame_LanceIndustries/Assets/Scripts/PrototypeScripts/Raycast.cs
+++ UnityGame_LanceIndustries/Assets/Scripts/PrototypeScripts/Raycast.cs
@@ -7,11 +7,6 @@ public class Raycast : MonoBehaviour
     void OnMouseDown()
     {
         rotateReflector((transform.rotation.eulerAngles.z));
-        Debug.Log("ADASDAS");
-        Debug.Log("aswe");
-        Debug.Log("wer");
-        Debug.LogWarning("WARNINGASFDA");
-        Debug.Log("TESTSTASAD");
     }
 
 
@@ -25,8 +20,4 @@ public class Raycast : MonoBehaviour
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
     }
 
-    public void TestFunc()
-    {
-        Debug.Log("COOL");
-    }
 }
