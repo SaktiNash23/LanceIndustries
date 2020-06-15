ed335c47977701fc2c5e87170a79ceafee54f6e0
Jack Chen Hong Jun <jack55769@codedecipher.co>
2020-06-15 22:12:37 +0800

    - Remove "THIS IS BS" comment.

--- UnityGame_LanceIndustries/Assets/Scripts/MapEditor/Manager/MapEditorManager.cs
+++ UnityGame_LanceIndustries/Assets/Scripts/MapEditor/Manager/MapEditorManager.cs
@@ -204,8 +204,6 @@ public class MapEditorManager : MonoBehaviour
 
         mapLayoutGameObj.SetActive(true);
 
-        Debug.Log("THIS IS BS");
-
         TextAsset mapData = AssetDatabase.LoadAssetAtPath<TextAsset>(LoadedMapDataPath);
         string jsonData = mapData.text;
         MapDataHolder mapDataHolder = JsonUtility.FromJson<MapDataHolder>(jsonData);
