d3d4c84e57e169bb045693ee1dac287f57b7752b
SaktiNash23 <sativel@hotmail.com>
2020-09-19 20:45:02 +0800

    - Implemented script to scale tutorial UI to different aspect ratios on awake

---
+++ UnityGame_LanceIndustries/Assets/Scripts/PrototypeScripts/TutorialDynamicScaler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Video;
+
+public class TutorialDynamicScaler : MonoBehaviour
+{
+    public GameObject content;
+    float aspectRatio = 0.0f;
+    public int numberOfTutorialPanels = 0;
+
+    private void Awake()
+    {
+        Debug.Log("Aspect : " + Camera.main.aspect);
+        aspectRatio = Camera.main.aspect;
+
+        if(aspectRatio > 0.55f)
+        {
+            content.GetComponent<RectTransform>().sizeDelta = new Vector2(1080.0f * numberOfTutorialPanels, 1920.0f);
+        }
+        else if(aspectRatio > 0.49f && aspectRatio < 0.51f)
+        {
+            content.GetComponent<RectTransform>().sizeDelta = new Vector2(1080.0f * numberOfTutorialPanels, 2160.0f);
+        }
+        else if(aspectRatio > 0.47f && aspectRatio < 0.49f)
+        {
+            content.GetComponent<RectTransform>().sizeDelta = new Vector2(1080.0f * numberOfTutorialPanels, 2280.0f);
+        }
+    }
+}
